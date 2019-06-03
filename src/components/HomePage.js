b25fa801d23de21c5e30a53c4ae1c9c8c336bd7b
Joyce Obi <joyceobi@Joyces-MacBook-Pro.local>
2019-06-03 12:41:09 +0100

    reduce signup buton size

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -33,26 +33,28 @@ const HomePage = () => (
         <div className="box">
           <i className="fas fa-user-shield" />
           <h3>Secure Voting</h3>
-          <p className="white">It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <p className="white"> It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages</p>
         </div>
         <div className="box">
           <i className="far fa-handshake" />
           <h3>interact With Candidates</h3>
-          <p className="white">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</p>
+          <p className="white"> Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
         </div>
         <div className="box">
           <i className="fas fa-vote-yea" />
           <h3>Get Results</h3>
-          <p className="white">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</p>
+          <p className="white"> Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</p>
         </div>
         <div className="box">
           <i className="fas fa-mobile-alt" />
           <h3>Get on the go</h3>
-          <p className="white"> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries.</p>
+          <p className="white"> ext of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it </p>
         </div>
       </div>
     </section>
-    {/* <Footer /> */}
+    <footer id="footer-hommy">
+      <p id="footer-p">&#169;Joyce-O Designs ~ 2019 General Elections</p>
+    </footer>
   </div>
 );
 
